f0c7bae44dda415ba1f8b26f0bbfa7f93945fd53
Duncan Morais <duncanmorais@gmail.com>
2026-02-25 20:52:13 -0700

    removed unecessary exports

--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
 export { FuzzyDate } from './fuzzyDate/fuzzyDate.js';
-export { FuzzyDateJson } from './fuzzyDate/helpers/types.js';
-export { fuzzyDateJsonSchema } from './fuzzyDate/helpers/schemas.js';
